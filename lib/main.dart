6eee5dd425de77afbdb3a0e9d157ef59f8ccb762
211110476_jimmy <211110476@students.mikroskil.ac.id>
2023-09-28 11:48:22 +0700

    menambah shared preferences(masih basic punya)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'botnav.dart';
+import 'package:tugaskelompok/utils/key_sharedpreference.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,9 +16,16 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Planner'),
+      home: GetStart(),
     );
   }
+}
+
+class GetStart extends StatefulWidget {
+  const GetStart({super.key});
+
+  @override
+  State<GetStart> createState() => _GetStartState();
 }
 
 class MyHomePage extends StatefulWidget {
@@ -26,6 +35,52 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _GetStartState extends State<GetStart> {
+  late SharedPreferences _pref;
+  bool pernahMasuk = false;
+
+  void checkApp() async {
+    _pref = await SharedPreferences.getInstance();
+    pernahMasuk = _pref.getBool(KeyStore.pernahMasuk) ?? false;
+    if (pernahMasuk == true) {
+      Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) {
+          return const MyHomePage(title: 'Planner');
+        },
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    checkApp();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Get Start Screen")),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              _pref = await SharedPreferences.getInstance();
+              _pref.setBool(KeyStore.pernahMasuk, true);
+              Navigator.pushReplacement(context, MaterialPageRoute(
+                builder: (context) {
+                  return MyHomePage(
+                    title: "Planner",
+                  );
+                },
+              ));
+            },
+            child: const Text("Get Start")),
+      ),
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
---
+++ lib/utils/key_sharedpreference.dart
@@ -0,0 +1,4 @@
+class KeyStore {
+  static String value1 = "VALUE_1";
+  static String pernahMasuk = "PERNAH_MASUK";
+}
