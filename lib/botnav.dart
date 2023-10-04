ece369bb55999c9b5360503c7da5f82432ccbafb
whojeee <211110335@students.mikroskil.ac.id>
2023-10-04 15:52:46 +0700

    Kategori di Homepage dan Calendar

--- lib/botnav.dart
+++ lib/botnav.dart
@@ -1,23 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-// Define kelas-kelas halaman
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController pageController = PageController(initialPage: 0);
+  int currentIndex = 0;
+
+  String getTitle(int index) {
+    switch (index) {
+      case 0:
+        return 'Today';
+      case 1:
+        return 'Week';
+      case 2:
+        return 'Month';
+      case 3:
+        return 'Year';
+      default:
+        return '';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    pageController.animateToPage(
+                      index,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Text(
+                      getTitle(index),
+                      style: TextStyle(
+                        color:
+                            currentIndex == index ? Colors.black : Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: PageView(
+              controller: pageController,
+              children: <Widget>[
+                Center(child: Text('Today Page')),
+                Center(child: Text('Week Page')),
+                Center(child: Text('Month Page')),
+                Center(child: Text('Year Page')),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class CalendarPage extends StatelessWidget {
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final bool isSelected;
+  final Function onTap;
+
+  CategoryCard({
+    required this.title,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap as void Function()?,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: isSelected ? Colors.black : Colors.grey,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            decoration: isSelected ? TextDecoration.underline : null,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CalendarPage extends StatefulWidget {
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+    });
+  }
+
+  String getEventText() {
+    if (_selectedDay != null) {
+      return "Tidak Ada Kegiatan";
+    } else {
+      return "";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Calendar Page'),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: _onDaySelected,
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+          SizedBox(height: 20),
+          Text(
+            getEventText(),
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
