890801e1fa0b263ad9c36388726672b357ffe761
whojeee <211110335@students.mikroskil.ac.id>
2023-10-22 01:40:10 +0700

    Kalendar hari merah dari API done

--- lib/Pages/Calendar.dart
+++ lib/Pages/Calendar.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyData {
+  final int id;
+  final String information;
+  final String day;
+  final String holidayDate;
+
+  MyData({
+    required this.id,
+    required this.information,
+    required this.day,
+    required this.holidayDate,
+  });
+
+  factory MyData.fromJson(Map<String, dynamic> json) {
+    return MyData(
+      id: json['id'],
+      information: json['information'],
+      day: json['day'],
+      holidayDate: json['holiday_date'],
+    );
+  }
+}
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -10,6 +35,7 @@ class _CalendarPageState extends State<CalendarPage> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  List<MyData> holidayData = [];
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
@@ -19,9 +45,78 @@ class _CalendarPageState extends State<CalendarPage> {
 
   String getEventText() {
     if (_selectedDay != null) {
-      return "Tidak Ada Kegiatan";
+      final selectedDate =
+          "${_selectedDay!.year}-${_selectedDay!.month.toString().padLeft(2, '0')}-${_selectedDay!.day.toString().padLeft(2, '0')}";
+
+      final eventData = holidayData.firstWhere(
+        (data) => data.holidayDate == selectedDate,
+        orElse: () => MyData(
+          id: -1,
+          information: "Tidak Ada Kegiatan",
+          day: "",
+          holidayDate: selectedDate,
+        ),
+      );
+
+      return eventData.information;
     } else {
       return "";
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+    fetchData2(); // Panggil fungsi fetchData2024
+  }
+
+  void fetchData() async {
+    final apiUrl = 'https://kasekiru.com/api/liburan/oG37i2GyVq64zRGI';
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final dynamic jsonResponse = json.decode(response.body);
+
+      if (jsonResponse is Map && jsonResponse.containsKey("data")) {
+        final data = jsonResponse["data"];
+        if (data is List) {
+          setState(() {
+            holidayData = data.map((item) => MyData.fromJson(item)).toList();
+          });
+        } else {
+          throw Exception('API response data is not a JSON array.');
+        }
+      } else {
+        throw Exception('API response is missing the "data" key.');
+      }
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  void fetchData2() async {
+    final apiUrl = "https://kasekiru.com/api/liburan/JeiaFN39De20Snra";
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final dynamic jsonResponse = json.decode(response.body);
+
+      if (jsonResponse is Map && jsonResponse.containsKey("data")) {
+        final data = jsonResponse["data"];
+        if (data is List) {
+          setState(() {
+            holidayData
+                .addAll(data.map((item) => MyData.fromJson(item)).toList());
+          });
+        } else {
+          throw Exception('API response data is not a JSON array.');
+        }
+      } else {
+        throw Exception('API response is missing the "data" key.');
+      }
+    } else {
+      throw Exception('Failed to load data');
     }
   }
 
@@ -49,10 +144,12 @@ class _CalendarPageState extends State<CalendarPage> {
             },
           ),
           SizedBox(height: 20),
-          Text(
-            getEventText(),
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
+          Container(
+              height: 200,
+              child: ListTile(
+                leading: Icon(Icons.calendar_today),
+                title: Text(getEventText()),
+              )),
         ],
       ),
     );
--- lib/Pages/Features.dart
+++ lib/Pages/Features.dart
@@ -2,131 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class MyData {
-  final int id;
-  final String information;
-  final String day;
-  final String holidayDate;
-
-  MyData({
-    required this.id,
-    required this.information,
-    required this.day,
-    required this.holidayDate,
-  });
-
-  factory MyData.fromJson(Map<String, dynamic> json) {
-    return MyData(
-      id: json['id'],
-      information: json['information'],
-      day: json['day'],
-      holidayDate: json['holiday_date'],
-    );
-  }
-}
-
-class GetHoliday extends StatefulWidget {
-  final List<String> apiLinks;
-
-  GetHoliday({required this.apiLinks});
-
-  @override
-  _GetHolidayState createState() => _GetHolidayState();
-}
-
-class _GetHolidayState extends State<GetHoliday> {
-  late String selectedApiLink;
-
-  @override
-  void initState() {
-    super.initState();
-    // Set the default API link when the widget initializes
-    selectedApiLink = widget.apiLinks[0];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Holiday List'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            // Go back to the previous screen (Features)
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: ListView(
-        children: [
-          // Generate chips based on the provided API links
-          Wrap(
-            children: widget.apiLinks.map((apiLink) {
-              final year = apiLink == selectedApiLink ? '2023' : '2024';
-              return ActionChip(
-                label: Text(year),
-                onPressed: () {
-                  // Update the selected API link
-                  setState(() {
-                    selectedApiLink = apiLink;
-                  });
-                },
-              );
-            }).toList(),
-          ),
-          // Add a container for displaying holiday data
-          Container(
-            child: FutureBuilder<List<MyData>>(
-              future: fetchData(selectedApiLink),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  final data = snapshot.data;
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    itemCount: data!.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(data[index].information),
-                        subtitle: Text(data[index].holidayDate),
-                      );
-                    },
-                  );
-                }
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<List<MyData>> fetchData(String apiUrl) async {
-    final response = await http.get(Uri.parse(apiUrl));
-
-    if (response.statusCode == 200) {
-      final dynamic jsonResponse = json.decode(response.body);
-
-      if (jsonResponse is Map && jsonResponse.containsKey("data")) {
-        final data = jsonResponse["data"];
-        if (data is List) {
-          return data.map((item) => MyData.fromJson(item)).toList();
-        } else {
-          throw Exception('API response data is not a JSON array.');
-        }
-      } else {
-        throw Exception('API response is missing the "data" key.');
-      }
-    } else {
-      throw Exception('Failed to load data');
-    }
-  }
-}
-
 class Features extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -135,17 +10,11 @@ class Features extends StatelessWidget {
         crossAxisCount: 2, // 2 cards per row
         children: [
           _FeatureCard(
-            title: "Holiday's date",
-            color: Colors.blue,
-            borderColor: Colors.greenAccent,
+            title: 'Feature 1',
+            color: Colors.red,
+            borderColor: Colors.orange,
             onTap: () {
-              // Navigate to the GetHoliday screen
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return GetHoliday(apiLinks: [
-                  "https://kasekiru.com/api/liburan/oG37i2GyVq64zRGI", // 2023
-                  "https://kasekiru.com/api/liburan/JeiaFN39De20Snra", // 2024
-                ]);
-              }));
+              // Handle tap on Feature 2
             },
             icon: Icons.star,
           ),
